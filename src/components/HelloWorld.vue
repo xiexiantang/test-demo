1863d33d37f84578bc5d17001abbf2948ecd6e47
xiexiantang <v_xiexiantang@baidu.com>
2026-03-23 12:22:59 +0800

    change page style

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import viteLogo from '../assets/vite.svg'
-import heroImg from '../assets/hero.png'
 import vueLogo from '../assets/vue.svg'
 
 const count = ref(0)
@@ -9,15 +8,6 @@ const count = ref(0)
 
 <template>
   <section id="center">
-    <div class="hero">
-      <img :src="heroImg" class="base" width="170" height="179" alt="" />
-      <img :src="vueLogo" class="framework" alt="Vue logo" />
-      <img :src="viteLogo" class="vite" alt="Vite logo" />
-    </div>
-    <div>
-      <h1>Get started</h1>
-      <p>Edit <code>src/App.vue</code> and save to test <code>HMR</code></p>
-    </div>
     <button class="counter" @click="count++">Count is {{ count }}</button>
   </section>
 
